84827e523e0ad70e2ab7e062654997c1fc91f2df
Matteus Isaque <matteus.isaque28@gmail.com>
2023-07-24 20:29:57 -0300

    alt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -97,6 +97,8 @@ export default function Page() {
 
 
 
+
+
   return (
     <main className="flex items-center justify-center min-h-screen">
       <form
--- src/app/painel/page.tsx
+++ src/app/painel/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "@/util/firebase";
 import CreateTask from "@/components/CreateTask";
@@ -5,12 +6,25 @@ import { useEffect, useState } from "react";
 import FindAllTask from "@/components/FindAllTask";
 import { IoMdAdd } from "react-icons/io";
 import Link from "next/link";
+import { getAuth, signOut } from "firebase/auth";
 
 export default function Index() {
+  function HandleSignOut() {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  }
+
   return (
     <main>
       <div className="mx-[10%] mt-10 bg-black-600 w-[80%] h-[1px]"></div>
       <div className="mx-[10%] mt-10 w-[80%] relative pt-16">
+        <button className="px-40 bg-black-100" onClick={HandleSignOut}>sair</button>
         <Link href="/painel/tarefas/nova">
           <div className="absolute top top-0 right-0 w-14 h-14 flex justify-center items-center rounded-lg bg-yellow-800 cursor-pointer hover:bg-yellow-900 transition">
             <IoMdAdd size={30} />
